b8abef7325a2491af97a6e3747ee6cfa5dc2a5ea
srinuv9696-max <srinuv9696@gmail.com>
2026-04-23 17:40:35 +0530

    Create Semi-Finished Goods management class

---
+++ modules/SemiFinishedGoods.php
@@ -0,0 +1,12 @@
+<?php
+class SemiFinishedGoods {
+    // Properties
+    private $id;
+    private $name;
+    private $quantity;
+    private $date_in;
+    private $date_out;
+
+    // Constructor, getters, and setters
+}
+?>
